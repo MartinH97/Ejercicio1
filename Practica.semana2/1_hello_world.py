48f0e461069390739d8f63a57f26302a1ddaf64d
martinh97 <spceclcl_96@hotmail.com>
2020-08-11 19:41:22 -0500

    practica.semana2

---
+++ Practica.semana2/1_hello_world.py
@@ -0,0 +1,6 @@
+nombre= str(input("Ingrese un nombre: "))  
+##
+if nombre == '':
+    print("Hello World!")
+else: 
+    print(f"Hello {nombre}!")
---
+++ Practica.semana2/3_km.py
@@ -0,0 +1,5 @@
+distancia= float(input("Ingrese la cantidad de distancia: "))
+tiempo= float(input("Ingrese el tiempo: ")) 
+##
+velocidad=  distancia/tiempo 
+print(f"La velocidad es de: {velocidad}km.h") 
